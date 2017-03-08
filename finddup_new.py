a0386fa2c5382c031f6ef76b77cbac1bdeca5382
Matt Clapp <itsayellow@gmail.com>
2017-03-07 23:26:59 -0800

    deleted a lot of old hash stuff

--- finddup_new.py
+++ finddup_new.py
@@ -15,7 +15,6 @@ import stat
 import os.path
 import sys
 import argparse
-import hashlib
 import time
 import subprocess
 import re
@@ -73,222 +72,6 @@ def process_command_line(argv):
 
     return args
 
-# TODO: hangs trying to open
-#   ~/Library/Application Support/LastPass/pipes/lastpassffplugin
-#   TODO: don't open pipes (use os.path.isfile() ??)
-def hash_file( filepath, am_verbose ):
-    hasher = hashlib.md5()   # 55sec/682 images
-    #hasher = hashlib.sha256() # 62sec/682 images
-    blocksize = 65536;
-
-    # only look at regular files that are not symbolic links
-    if os.path.isfile( filepath ) and not os.path.islink( filepath ):
-        try:
-            fp = open( filepath, 'rb')
-        except OSError:
-            #sys.stderr.write("Can't read: "+filepath+"\n")
-            print("Can't read: "+filepath)
-            return "-1"
-        #sys.stderr.write("Reading "+filepath+"\n")
-        try:
-            buf = fp.read(blocksize)
-        except OSError:
-            fp.close()
-            #sys.stderr.write("Problem reading: "+filepath+"\n")
-            print("Problem reading: "+filepath)
-            return "-1"
-        while len(buf) > 0:
-            hasher.update(buf)
-            buf = fp.read(blocksize)
-        fp.close()
-        # TODO: return hex instead for compactness
-        return hasher.hexdigest()
-    else:
-        #sys.stderr.write("Not a regular file: "+filepath+"\n")
-        if am_verbose:
-            print("Not a regular file: "+filepath)
-        return "-1"
-
-
-# TODO: hangs trying to open
-#   ~/Library/Application Support/LastPass/pipes/lastpassffplugin
-#   TODO: don't open pipes (use os.path.isfile() ??)
-def hash_file_map( filepath, am_verbose ):
-    hasher = hashlib.md5()   # 55sec/682 images
-    #hasher = hashlib.sha256() # 62sec/682 images
-    blocksize = 65536;
-
-    # only look at regular files that are not symbolic links
-    if os.path.isfile( filepath ) and not os.path.islink( filepath ):
-        try:
-            fp = open( filepath, 'rb')
-        except OSError:
-            #sys.stderr.write("Can't read: "+filepath+"\n")
-            print("Can't read: "+filepath)
-            this_hash = "-1"
-        else:
-            #sys.stderr.write("Reading "+filepath+"\n")
-            try:
-                buf = fp.read(blocksize)
-            except OSError:
-                fp.close()
-                #sys.stderr.write("Problem reading: "+filepath+"\n")
-                print("Problem reading: "+filepath)
-                this_hash = "-1"
-            else:
-                while len(buf) > 0:
-                    hasher.update(buf)
-                    buf = fp.read(blocksize)
-                fp.close()
-                # TODO: return hex instead for compactness
-                this_hash = hasher.hexdigest()
-    else:
-        #sys.stderr.write("Not a regular file: "+filepath+"\n")
-        if am_verbose:
-            print("Not a regular file: "+filepath)
-        this_hash = "-1"
-
-    return (filepath, this_hash)
-
-# recurse
-# filetree is a dict of dicts
-# treeroot is the top of a tree (don't split it further)
-# root is current root (some of which is treeroot)
-# return a dict
-def return_dict(filetree,treeroot,root):
-    if treeroot == root:
-        # if we've reached the root of the tree, then recursion stops, return
-        #   the dict
-        # if dict doesn't exist, then return empty dict
-        if treeroot not in filetree:
-            filetree[treeroot] = {}
-        return filetree[treeroot]
-    else:
-        (root1,root2) = os.path.split(root)
-        filetree_branch = return_dict( filetree, treeroot, root1)
-        if root2 not in filetree_branch:
-            filetree_branch[root2]={}
-        return filetree_branch[root2]
-
-
-# input paths is list of paths to be searched recursively
-# input uniquefiles are files that have unique size and don't need to be hashed
-# input am_verbose is option to print more messages
-# output all_hashes is dict, keys are full filepaths, items are hash values
-def get_all_hashes( paths, uniquefiles, am_verbose ):
-    all_hashes = {}
-    files_tohash = []
-
-    # first get list of all files that need to be hashed
-    for treeroot in paths:
-        if os.path.isdir( treeroot ):
-            for (root,dirs,files) in os.walk(treeroot):
-                for filename in files:
-                    if IGNORE_FILES.get(filename,False):
-                        continue
-                    filepath = os.path.join(root,filename)
-                    if filepath in uniquefiles:
-                        # guaranteed not to match hash_file return of pure hex
-                        all_hashes[filepath] = "size:"+str(uniquefiles[filepath])
-                    else:
-                        files_tohash.append(filepath)
-        else:
-            print( "skipping file (TODO) "+treeroot)
-
-    # multiprocessing speeds up by about x2 - x3 for 8-processor
-    #   somewhat file access limited rather than processor limited
-    with multiprocessing.Pool() as filehash_pool:
-        files_hashes = filehash_pool.map(
-                partial(
-                    hash_file_map,
-                    am_verbose=am_verbose
-                    ),
-                files_tohash
-                )
-
-    all_hashes.update( dict(files_hashes) )
-    return all_hashes
-
-
-def make_hashes( paths, all_hashes, uniquefiles, am_verbose ):
-    filetree={}
-    filesreport_time = time.time()
-    needs_cr = False
-    for treeroot in paths:
-        if needs_cr:
-            sys.stderr.write("\n")
-            needs_cr = False
-        sys.stderr.write("Starting hashing of: "+treeroot+"\n")
-        # remove trailing slashes, etc.
-        treeroot = os.path.normpath(treeroot)
-        if os.path.isdir( treeroot ):
-            filesdone = 0
-            for (root,dirs,files) in os.walk(treeroot):
-                for filename in files:
-                    # TODO: experiemental
-                    if IGNORE_FILES.get(filename,False):
-                        continue
-                    filepath = os.path.join(root,filename)
-                    # TODO: the following has thrown a KeyError, why, how?
-                    #   did the file get created since we made all_hashes?
-                    this_hash = all_hashes[filepath]
-                    filesdone+=1
-                    if this_hash != "-1":
-                        return_dict(filetree,treeroot,root)[filename]=this_hash
-                    else:
-                        if am_verbose:
-                            print( "Not adding to dict: "+filename+this_hash)
-                    if filesdone%100 == 0 or time.time()-filesreport_time > 15:
-                        sys.stderr.write("\b"*40+"  "+str(filesdone)+" files hashed.")
-                        sys.stderr.flush() #py3 doesn't seem to flush until \n
-                        needs_cr = True
-                        filesreport_time = time.time()
-            sys.stderr.write("\b"*40+"  "+str(filesdone)+" files hashed.\n")
-            needs_cr = False
-        else:
-            print( "skipping file (TODO) "+treeroot)
-
-    return filetree
-
-
-# if is file (hex string) add hex string to db, then return string
-# if is dir (dict): sort keys, concatenate all
-def recurse_filetree( filetree, all_hashes, path_parent ):
-    filetree_str = ""
-    for filedir in sorted(filetree.keys()):
-        if type(filetree[filedir]) is dict:
-            this_str = "["
-            this_str += recurse_filetree(
-                    filetree[filedir],
-                    all_hashes,
-                    os.path.join(path_parent,filedir)
-                    )
-            this_str += "]"
-            # represents a directory, return [hashes]
-            all_hashes[this_str] = all_hashes.get(this_str,[]) + [os.path.join(path_parent,filedir)+os.path.sep,]
-        else:
-            # represents a file, return hash
-            this_str = filetree[filedir]
-            all_hashes[this_str] = all_hashes.get(this_str,[]) + [os.path.join(path_parent,filedir),]
-        filetree_str += this_str
-    return filetree_str
-
-# filetree is a dict of all files or directories at this level.  Each key
-#   resolves to either a hex string or another dict (for a directory)
-# e.g. subtree = filetree['.']
-#   subtree['readme.txt']=9384920fa8d8x
-#   subsubtree = subtree['subdir']
-def filetree2hashes( filetree ):
-    all_hashes = {}
-
-    # this filetree_str is bogus: as if all cmdline dirs were in same dir
-    filetree_str = recurse_filetree( filetree, all_hashes, "")
-    return all_hashes
-
-def print_hashes( all_hashes ):
-    for hash in all_hashes.keys():
-        print( hash )
-        print( all_hashes[hash] )
 
 def k2prefix( size_kb ):
     if size_kb > 1024*1024:
@@ -626,29 +409,16 @@ def main(argv=None):
     #   1. For every file, get: size, mod_time
     #   2. hash by sizes, each hash size in dict fill with list of all files that size
     #   3. go through all hashes, for each hash deciding which of the list are unique or same
+    #       by comparing matching-size files block by block, splitting into groups
 
-    # filesizes is dict: keys are file sizes in bytes, items are lists of 2-tuples (filepath,modtime) that
-    #   are all that size
-    # unique_size_files is dict: keys are filepath, items are sizes (TODO: maybe not useful??)
+    # filesizes is dict: keys are file sizes in bytes, items are lists of filepaths that
+    #   are all that size (lists are all len > 1)
+    # unique_size_files is list: filepaths
+    # filemodtimes is dict: keys are filepaths, items are modtimes when file was stat'ed
     (filesizes, unique_size_files, filemodtimes) = hash_files_by_size( args.searchpaths )
 
-    # TODO: filesizes hash also contains verified unique files, we need to omit them from compare_files
+    # compare all filegroups by using byte-by-byte comparison to find actually unique, duplicate
     (dupgroups,uniquefiles,invalid_files) = compare_files(filesizes)
-
-    # testing
-    mytimer.start()
-    #all_files_hashes = get_all_hashes( args.searchpaths, uniquefiles, args.verbose )
-    mytimer.eltime_pr("get_all_hashes: ", prfile=sys.stderr )
-    #print( "all_files_hashes" )
-    #print( all_files_hashes )
-
-    #filetree = make_hashes( args.searchpaths, all_files_hashes, uniquefiles, args.verbose )
-
-    #all_hashes = filetree2hashes( filetree )
-
-    #print_hashes( all_hashes )
-
-    #analyze_hashes( all_hashes )
 
     mytimer2.eltime_pr("Elapsed time: ", prfile=sys.stderr )
     mytimer2.eltime_pr("Elapsed time: ", prfile=sys.stdout )
