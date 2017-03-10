abf3c59870d9f261ee3ee516d3d8434467252651
Matt Clapp <itsayellow@gmail.com>
2017-03-09 23:39:38 -0800

    report pretty much matches finddup.py almost done

--- finddup_new.py
+++ finddup_new.py
@@ -86,66 +86,34 @@ def k2prefix( size_kb ):
     else:
         return "%.fkB"%(size_kb)
 
-def analyze_hashes( all_hashes ):
-    unique_files = []
-    dup_files = []
-
-    print("Analyzing files...", file=sys.stderr)
-
-    for key in all_hashes.keys():
-        if len(all_hashes[key]) == 1:
-            unique_files.extend(all_hashes[key])
-        else:
-            filedir = all_hashes[key][0]
-            if os.path.isfile(filedir):
-                # do it the quick way if we are a regular file
-                #   off from du by at most 4k
-                dup_size = int( os.path.getsize(filedir) / 1024.0 )
-            else:
-                dup_size = subprocess.check_output(["du","-sk",filedir])
-                dup_size = dup_size.decode("utf-8")
-                dup_size = int(re.search(r'\d+',dup_size).group(0))
-            dup_files.append([dup_size,]+all_hashes[key])
-
-    dup_files.sort(reverse=True, key=lambda dup_list: dup_list[0])
-    unique_files.sort()
-
-    print( "Duplicate Files/Directories:" )
-    for filelist in dup_files:
-        print( "Duplicate set (%s each)"%k2prefix(filelist[0]) )
-        for filename in filelist[1:]:
-            print( "  "+filename )
-
-    print( "\n" )
-    print( "Unique Files/Directories:" )
-    for filename in unique_files:
-        print( filename )
-
 
 # get filestat on file if possible (i.e. readable), discard if symlink, pipe, fifo
-# from filestat return filesize, file mod_time
+# from filestat return filesize, file mod_time, file blocks
 # return (-1,-1) if discarded file
 def check_stat_file(filepath):
     # TODO: experimental
     if IGNORE_FILES.get(os.path.basename(filepath),False):
-        return (-1,-1)
+        return (-1,-1,-1)
 
     # TODO: I think we want to skip links to files but need to confirm
     # skip symbolic links without commenting
-    if os.path.islink(filepath):
-        return (-1,-1)
+    # TODO: do we still want to record blocks for directory tally?
+    # ACTUALLY I THINK WE WANT TO TREAT SYMLINKS LIKE A REGULAR FILE and not follow what they're pointing to
+    #if os.path.islink(filepath):
+    #    return (-1,-1,-1)
 
     try:
-        this_filestat = os.stat(filepath)
+        # don't follow symlinks, just treat them like a regular file
+        this_filestat = os.stat(filepath, follow_symlinks=False)
     except OSError as e:
         # e.g. FileNotFoundError, PermissionError
         if needs_cr:
             print("", file=sys.stderr)
             needs_cr = False
-        print("Filestat Error opening: "+filepath, file=sys.stderr)
+        print("Filestat Error opening:\n"+filepath, file=sys.stderr)
         print("  Error: "+str(type(e)), file=sys.stderr )
         print("  Error: "+str(e), file=sys.stderr )
-        return (-1,-1)
+        return (-1,-1,-1)
     except:
         e = sys.exc_info()
         # TODO: use stderr, check and later set needs_cr
@@ -156,24 +124,26 @@ def check_stat_file(filepath):
         print("  Error: "+str(e[0]), file=sys.stderr )
         print("  Error: "+str(e[1]), file=sys.stderr )
         print("  Error: "+str(e[2]), file=sys.stderr )
-        return (-1,-1)
+        return (-1,-1,-1)
 
     # skip FIFOs without commenting
     if stat.S_ISFIFO(this_filestat.st_mode):
-        return (-1,-1)
+        return (-1,-1,-1)
     # skip sockets without commenting
     if stat.S_ISSOCK(this_filestat.st_mode):
-        return (-1,-1)
+        return (-1,-1,-1)
 
     this_size = this_filestat.st_size
     this_mod = this_filestat.st_mtime
+    this_blocks = this_filestat.st_blocks
     
-    return (this_size, this_mod)
+    return (this_size, this_mod, this_blocks)
 
 
 # filetree is dict of dicts and items
 #   each subdir is a nested dict subtree containing dicts and items
 #   base of filetree corresponds to master_root
+#   file item is [file_id, size]
 # return valid pointer to subtree of tree corresponding to root dir
 # base of dict tree is relative to string master_root
 # root is string of dir to get dict of (also containing master_root string)
@@ -188,6 +158,7 @@ def subtree_dict(filetree, root, master_root):
     subtree = filetree
     for pathpart in root_relative.split( os.path.sep ):
         if pathpart and pathpart != '.':
+            # either get pathpart key of subtree or create new one (empty dict)
             subtree = subtree.setdefault(pathpart,{})
     return subtree
 
@@ -195,13 +166,15 @@ def subtree_dict(filetree, root, master_root):
 # go through every file hierarchically inside argument paths
 # make hash with keys being filesize in bytes and value being list of files that match
 def hash_files_by_size( paths, master_root ):
-    filesizes = {}
+    file_size_hash = {}
     filetree = {}
+    fileblocks = {}
     filemodtimes = {}
     filesreport_time = time.time()
     needs_cr = False
     filesdone = 0
 
+    #.........................
     # local function to process one file
     def process_file_size():
         # read/write these from hash_files_by_size scope
@@ -211,13 +184,14 @@ def hash_files_by_size( paths, master_root ):
         subtree_dict(filetree, root, master_root)[filename] = -1
 
         filepath = os.path.join(root,filename)
-        (this_size, this_mod) = check_stat_file(filepath)
+        (this_size, this_mod, this_blocks) = check_stat_file(filepath)
         if this_size == -1:
             return
 
         # setdefault returns [] if this_size key is not found
-        # append as item to filesizes [filepath,filemodtime] to check if modified later
-        filesizes.setdefault(this_size,[]).append(filepath)
+        # append as item to file_size_hash [filepath,filemodtime] to check if modified later
+        file_size_hash.setdefault(this_size,[]).append(filepath)
+        fileblocks[filepath] = this_blocks
         filemodtimes[filepath] = this_mod
 
         filesdone+=1
@@ -225,8 +199,9 @@ def hash_files_by_size( paths, master_root ):
             print( "\r  "+str(filesdone)+" files sized.", end='', file=sys.stderr, flush=True)
             filesreport_time = time.time()
             needs_cr = True
+    #.........................
 
-    # Actual hierarchical processing
+    # Actual hierarchical file stat processing
     for treeroot in paths:
         if needs_cr:
             print("", file=sys.stderr)
@@ -247,22 +222,18 @@ def hash_files_by_size( paths, master_root ):
         print("\r  "+str(filesdone)+" files sized.", file=sys.stderr)
         needs_cr = False
 
+    # tally unique, possibly duplicate files
     unique = 0
     nonunique = 0
-    uniquefiles = []
-    for key in filesizes.keys():
-        if len(filesizes[key])==1:
+    for key in file_size_hash.keys():
+        if len(file_size_hash[key])==1:
             unique += 1
-            uniquefiles.append(filesizes[key][0])
         else:
-            nonunique += len(filesizes[key])
-
-    # remove all unique filesize files from filesizes
-    filesizes = {k:filesizes[k] for k in filesizes if len(filesizes[k])>1}
-
+            nonunique += len(file_size_hash[key])
     print("\nUnique: %d    "%unique, file=sys.stderr)
     print("Possibly Non-Unique: %d\n"%nonunique, file=sys.stderr)
-    return (filesizes,uniquefiles,filemodtimes,filetree)
+
+    return (file_size_hash, filetree, filemodtimes, fileblocks)
 
 
 # datachunks_list: list of arrays
@@ -285,7 +256,12 @@ def matching_array_groups(datachunks_list):
     return match_groups
 
 
-def compare_file_group(filelist):
+# all files in filelist are of size file_size (in blocks)
+# dup_groups = [
+#                   [file_size1, [identical_file1a, identical_file1b, identical_file1c]],
+#                   [file_size2, [identical_file2a, identical_file2b]]
+#               ]
+def compare_file_group(filelist, fileblocks):
     #print('------ compare_file_group -----------')
     max_files_open = 1 # TODO: we can optimize by allowing multiple files open at the same time
 
@@ -297,6 +273,11 @@ def compare_file_group(filelist):
     unique_files = []
     dup_groups = []
     invalid_files = []
+
+    # check if this is too easy (only one file)
+    if len(filelist)==1:
+        return (filelist,[],[])
+        #return (unique_files,dup_groups,invalid_files)
 
     # right now only one prospective group of files, split later if distinct file groups are found
     filelist_groups_next=[filelist]
@@ -327,24 +308,24 @@ def compare_file_group(filelist):
                         thisfile_fh.seek(filepos)
                         this_filedata = thisfile_fh.read(amt_file_read)
                     filedata_list.append(this_filedata)
-                    # get how many bytes we actually read (may be less than max)
+                    # filedata_size_list is how many bytes we actually read (may be less than max)
                     filedata_size_list.append(len(this_filedata))
                 #except (FileNotFoundError, PermissionError) as e:
                 except OSError as e:
                     # e.g. FileNotFoundError, PermissionError
-                    print("Error opening: "+thisfile)
-                    print("  Error: "+str(type(e)) )
-                    print("  Error: "+str(e) )
+                    print("Error opening:\n"+thisfile, file=sys.stderr)
+                    print("  Error: "+str(type(e)), file=sys.stderr )
+                    print("  Error: "+str(e), file=sys.stderr )
                     invalid_files.append([ thisfile, str(type(e)), str(e) ])
                     # append -1 to signify invalid
                     filedata_list.append(-1)
                     filedata_size_list.append(-1)
                 except:
                     e = sys.exc_info()
-                    print("UNHANDLED Error opening: "+thisfile)
-                    print("  Error: "+str(e[0]))
-                    print("  Error: "+str(e[1]))
-                    print("  Error: "+str(e[2]))
+                    print("UNHANDLED Error opening:\n"+thisfile, file=sys.stderr)
+                    print("  Error: "+str(e[0]), file=sys.stderr)
+                    print("  Error: "+str(e[1]), file=sys.stderr)
+                    print("  Error: "+str(e[2]), file=sys.stderr)
                     raise e[0]
 
                     #invalid_files.append([ thisfile, str(e[0]), str(e[1]) ])
@@ -375,7 +356,9 @@ def compare_file_group(filelist):
                 if filedata_sizes_group[0] < amt_file_read:
                     # if entire matching group has less data than we tried to read, we are at end of files
                     #   and this is a final dupgroup
-                    dup_groups.append([filelist_group[i] for i in match_idx_group])
+                    this_dup_group_list = [filelist_group[i] for i in match_idx_group]
+                    this_dup_blocks = fileblocks[this_dup_group_list[0]]
+                    dup_groups.append([this_dup_blocks, this_dup_group_list])
                 else:
                     # if filedata size is amt_file_read then not at end of files, keep reading / checking
                     filelist_groups_next.append([filelist_group[i] for i in match_idx_group])
@@ -398,13 +381,14 @@ def compare_file_group(filelist):
     return (unique_files,dup_groups,invalid_files)
 
 
-def compare_files(filesizes):
+def compare_files(file_size_hash, fileblocks):
     unique_files = []
     dup_groups = []
     invalid_files = []
 
-    for key in filesizes.keys():
-        (this_unique_files,this_dup_groups,this_invalid_files) = compare_file_group(filesizes[key])
+    for key in file_size_hash.keys():
+        (this_unique_files,this_dup_groups,this_invalid_files
+                ) = compare_file_group(file_size_hash[key], fileblocks)
         unique_files.extend(this_unique_files)
         dup_groups.extend(this_dup_groups)
         invalid_files.extend(this_invalid_files)
@@ -412,6 +396,7 @@ def compare_files(filesizes):
     return (dup_groups,unique_files,invalid_files)
 
 
+# returns file_id dict with id item for every filepath key
 def create_file_ids(dup_groups, unique_files, invalid_files, filetree, master_root):
     file_id = {}
     idnum = 0;
@@ -424,7 +409,7 @@ def create_file_ids(dup_groups, unique_files, invalid_files, filetree, master_ro
 
         idnum += 1
     for dup_group in dup_groups:
-        for dup_file in dup_group:
+        for dup_file in dup_group[1]:
             file_id[dup_file] = idnum
 
             # add id to tree
@@ -460,14 +445,23 @@ def process_searchpaths( searchpaths ):
     return (master_root, new_searchpaths)
 
 
-def recurse_subtree(name, subtree, dir_dict):
+# name is filepath of this dir/file
+# subtree is dict of this dir/file
+def recurse_subtree(name, subtree, dir_dict, fileblocks):
     itemlist = []
+    dir_blocks = 0
     for key in subtree.keys():
+        # key is name of dir/file inside of this dir
         if type(subtree[key]) is dict:
-            item = recurse_subtree(os.path.join(name,key), subtree[key], dir_dict)
+            item = recurse_subtree(os.path.join(name,key), subtree[key], dir_dict, fileblocks)
         else:
             item = str(subtree[key])
+        dir_blocks += fileblocks[os.path.join(name,key)]
         itemlist.append(item)
+
+    # put file blocks back into fileblocks db
+    fileblocks[name] = dir_blocks
+
     # if any one item is "-1" (unknown file) then this whole directory is "-1"
     #   in this way we mark every subdir above unknown file as unknown
     if "-1" in itemlist:
@@ -476,14 +470,14 @@ def recurse_subtree(name, subtree, dir_dict):
         itemlist.sort()
         hier_id_str = '['+','.join(itemlist)+']'
 
-    print(name+":"+hier_id_str)
     dir_dict.setdefault(hier_id_str,[]).append(name)
 
     return hier_id_str
 
 
 # inventory directories based on identical/non-identical contents (ignoring file/dir names)
-def recurse_analyze_filetree(filetree, rootname):
+# dir_dict: key: hash based on dir hierarchical contents, item: dir path
+def recurse_analyze_filetree(filetree, master_root, fileblocks, dup_groups ):
     dup_dirs = []
     unique_dirs = []
     dir_dict = {}
@@ -492,15 +486,51 @@ def recurse_analyze_filetree(filetree, rootname):
     #   else because it is highest
     # recurse_subtree creates a string representation of every subdir represented in filetree, based on
     #   the a hierarchical concatenation of the file ids in each subdir's hierarchy of files
-    root_str = recurse_subtree(rootname, filetree, dir_dict)
+    root_str = recurse_subtree(master_root, filetree, dir_dict, fileblocks)
 
     # unknown dirs show up with key of "-1"
     unknown_dirs = dir_dict["-1"]
     del(dir_dict["-1"])
 
     dup_dirs = [dir_dict[x] for x in dir_dict.keys() if len(dir_dict[x]) > 1]
+    for i in range(len(dup_dirs)):
+        # convert blocks to bytes to compare with dup_files
+        dup_dirs[i] = [fileblocks[dup_dirs[i][0]],dup_dirs[i]]
+
     unique_dirs = [dir_dict[x][0] for x in dir_dict.keys() if len(dir_dict[x]) == 1]
-    return (dup_dirs,unique_dirs,unknown_dirs)
+
+    dup_groups.extend(dup_dirs)
+    return (unique_dirs,unknown_dirs)
+
+
+def print_sorted_dups(dup_groups, master_root):
+    print("")
+    print("Duplicate Files/Directories:")
+    for dup_group in sorted(dup_groups, reverse=True, key=lambda x: x[0]):
+        print("Duplicate set (%d bytes each)"%(512*dup_group[0]))
+        for filedir in dup_group[1]:
+            if master_root == "/":
+                # all paths are abspaths
+                print("  %s"%filedir)
+            else:
+                # relpath from master_root
+                print("  %s"%os.path.relpath(filedir, start=master_root))
+
+def print_sorted_uniques(unique_files, master_root):
+    print("\n")
+    print("Unique Files/Directories:")
+    for filedir in sorted(unique_files):
+        if master_root == "/":
+            # all paths are abspaths
+            print(filedir)
+        else:
+            # relpath from master_root
+            print(os.path.relpath(filedir, start=master_root))
+
+
+def print_header(master_root):
+    if master_root != "/":
+        print("All file paths referenced from:\n"+master_root)
 
 
 # IDEA
@@ -524,23 +554,17 @@ def main(argv=None):
     #       by comparing matching-size files block by block, splitting into subgroups as differences found
 
     (master_root, searchpaths) = process_searchpaths( args.searchpaths )
-    print( "searchpaths: "+str(searchpaths))
-    print( "master_root: "+master_root)
     
-    # filesizes is dict: keys are file sizes in bytes, items are lists of filepaths that
+    # file_size_hash is dict: keys are file sizes in bytes, items are lists of filepaths that
     #   are all that size (lists are all len > 1)
-    # unique_size_files is list: filepaths
     # filemodtimes is dict: keys are filepaths, items are modtimes when file was stat'ed
-    (filesizes, unique_size_files, filemodtimes, filetree) = hash_files_by_size( searchpaths, master_root )
+    ( file_size_hash, filetree, filemodtimes, fileblocks) = hash_files_by_size( searchpaths, master_root)
 
     # compare all filegroups by using byte-by-byte comparison to find actually unique, duplicate
     # dupgroups: list of lists of identical files
     # uniquefiles: list unique files
     # invalid_files: list problematic (mostly unreadable) files
-    (dup_groups,unique_files,invalid_files) = compare_files(filesizes)
-
-    # make sure all unique files are folded into list unique_files
-    unique_files.extend(unique_size_files)
+    (dup_groups,unique_files,invalid_files) = compare_files(file_size_hash, fileblocks)
 
     # now we know all of the files that are duplicates and unique
     # we will also determine below which directories are identical in that they contain identical
@@ -552,36 +576,26 @@ def main(argv=None):
     file_id = create_file_ids(dup_groups, unique_files, invalid_files, filetree, master_root)
 
     # inventory directories based on identical/non-identical contents (ignoring names)
-    (dup_dirs,unique_dirs,unknown_dirs) = recurse_analyze_filetree(filetree, master_root)
-    
-    print(filetree)
+    # unknown_dirs are any dir that we couldn't check any of the files due to file issues, etc.
+    # file_size_hash gives info on file sizes, used to compute directory total size
+    (unique_dirs, unknown_dirs) = recurse_analyze_filetree(filetree, master_root, fileblocks, dup_groups)
 
-    #all_hashes = filetree2hashes( filetree )
+    # TODO: double-check which files have changed during the preceding by stating modtime on all files
+    #       all over again, put changed files in "unknown" category?
 
-    #analyze_hashes( all_hashes )
+    # PRINT REPORT
 
-    # find matching directories containing sets of matching files
-    # also check if files have changed since start of analysis and denote them unknown
+    # header for report
+    print_header(master_root)
 
-    # do we need a unique id for each file that is item for filepath key?
-    #   then we could just go through tree, constructing dir key from contents keys
-    #   any file that is invalid or unknown invalidates all dirs above it
+    # print a sorted (biggest dir/files first) list of dup groups
+    print_sorted_dups(dup_groups, master_root)
 
+    # print a sorted (alphabetical) list of unique files and dirs
+    unique_files.extend(unique_dirs)
+    print_sorted_uniques(unique_files, master_root)
 
-    # final tally (DEBUG)
-    print("unique_files:")
-    print(unique_files)
-    print("\n\ndup_groups:")
-    for dup_group in dup_groups:
-        print(dup_group)
-    print("\n\ninvalid_files:")
-    print(invalid_files)
     print("")
-    print("\n\ndup_dirs")
-    print(dup_dirs)
-    print("\n\nunique_dirs")
-    print(unique_dirs)
-
     mytimer2.eltime_pr("Elapsed time: ", prfile=sys.stderr )
     mytimer2.eltime_pr("Elapsed time: ", prfile=sys.stdout )
     return 0
