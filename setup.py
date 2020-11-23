375fd654f3ab1ef301a14740e28d617c992f91b5
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-22 20:32:30 -0800

    Fix required python3.6 and markdown type.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 from setuptools import setup  # type: ignore
 
-if sys.version_info < (3, 5, 0):
+if sys.version_info < (3, 6, 0):
     sys.exit(
         "Python 3.6 or later is required. "
         "See https://github.com/pipxproject/pipx "
